c517bfa3f48d6a97be87fa4de3fb642dd3e357fd
hyeyeon0210 <57626569+hyeyeon0210@users.noreply.github.com>
2026-08-13 13:37:58 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 
   try {
 
@@ -140,4 +140,5 @@ export default async function handler(req, res) {
 
   }
 
+}
 }
